1a129436b6b95ea34c6b8454064723a337d972a5
Eugeny Burdo <yauheni.burdo@rabobank.nl>
2024-12-21 15:07:09 +0100

    add mock

--- petfinder-api/petfinder-api.js
+++ petfinder-api/petfinder-api.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+const mock1111 = require('./mock-data/111111111111111.json');
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/api/v1/barcode/:id', (req, res) => {
+  console.log(`Barcode id: ` + req.params.id)
+  res.json(mock1111) 
 })
 
 app.listen(port, () => {
