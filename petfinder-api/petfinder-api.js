a217cd8890320bb7e3226d3f5fdfc26d6966774b
ebd622 <eburdo@gmail.com>
2024-12-24 23:24:08 +0100

    add business logic and mocks

--- petfinder-api/petfinder-api.js
+++ petfinder-api/petfinder-api.js
@@ -4,6 +4,7 @@ const axios = require('axios')
 const cheerio = require('cheerio');
 const fs = require('fs');
 const path = require('path');
+//const { HttpsProxyAgent } = require('https-proxy-agent');
 
 const port = 3000
 
@@ -13,12 +14,18 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 const mock1111 = require('./mock-data/111111111111111.json');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('This is a PetFinder API')
 })
 
 app.get('/api/v1/barcode/:id', async (req, res) => {
   const barcodeId = req.params.id;
-  console.log(`Barcode id: ` + barcodeId)
+  console.log(`Provided Barcode id: ` + barcodeId)
+  // Validate barcodeId
+  const barcodeRegex = /^\d{15}$/;
+  if (!barcodeRegex.test(barcodeId)) {
+    return res.status(400).json({ message: 'Invalid barcode ID. It should be exactly 15 digits with no spaces.' });
+  }
+  
   if (barcodeId === '111111111111111') {
     return res.json(mock1111)
   }
@@ -31,23 +38,28 @@ app.get('/api/v1/barcode/:id', async (req, res) => {
     const html = readMockHtml(`mock-333333333333333.html`);
     const $ = cheerio.load(html);
     const message = $('h1').text().trim();
-    return res.json({ message });
+    return res.status(403).json({ message });
   }
 
   try {
     const response = await axios.get(`https://ndg.nl/frm/fsearch.php?barcode=${barcodeId}`);
     const html = response.data;
+    //console.log(`Response ` + html)
 
-    //const html = readMockHtml('mock-222222222222222.html');
-
-    console.log(`Response ` + html)
+    //Check for error message
+    const containsString = html.includes("Toegang geweigerd");
+    if(containsString){
+      console.log(`Toegang geweigerd!`)
+      return res.status(403).json({ message: 'Toegang geweigerd!' });
+    }
+    
+    //Convert html to json
     const data = htmlToJson(html);
     return res.json(data);
 
-
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error fetching data');
+    res.status(500).send({message: 'Error fetching data'});
   }
 
 })
@@ -56,6 +68,7 @@ app.get('/api/v1/barcode/:id', async (req, res) => {
 const htmlToJson = (html) => {
   const $ = cheerio.load(html);
 
+  //Extract data
   const data = {
     source: "NDG",
     description: "De Stichting Nederlandse Databank Gezelschapsdieren",
@@ -72,9 +85,6 @@ const htmlToJson = (html) => {
       Overleden: $('td.frmLabel:contains("Overleden?")').next('td.frmField').text()
     }
   };  
-
-  console.log( 'Identificatienummer: ' + $('#content') );
-
   return data;
 };
 
@@ -89,4 +99,3 @@ const readMockHtml = (file) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
