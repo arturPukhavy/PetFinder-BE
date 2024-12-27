101362577828c922c522c09487a308f0d812c2d9
ebd622 <eburdo@gmail.com>
2024-12-27 18:29:10 +0100

    update mocks

--- petfinder-api/petfinder-api.js
+++ petfinder-api/petfinder-api.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const axios = require('axios')
-const cheerio = require('cheerio');
-const fs = require('fs');
-const path = require('path');
-//const { HttpsProxyAgent } = require('https-proxy-agent');
+// const axios = require('axios')
+// const cheerio = require('cheerio');
+// const fs = require('fs');
+// const path = require('path');
+
 
 const port = 3000
 
@@ -25,79 +25,110 @@ app.get('/api/v1/barcode/:id', async (req, res) => {
   if (!barcodeRegex.test(barcodeId)) {
     return res.status(400).json({ message: 'Invalid barcode ID. It should be exactly 15 digits with no spaces.' });
   }
-  
+  if (barcodeId === '000000000000000') {
+    return res.status(404).json({ message: 'Pet not found' });
+  }
   if (barcodeId === '111111111111111') {
+    mock1111.zoekresultaat.Identificatienummer = barcodeId;
+    mock1111.zoekresultaat.Diernaam = 'Simba';
+    mock1111.zoekresultaat.Diersoort = 'Kat';
+    mock1111.zoekresultaat.Woonplaats_houder = 'Rotterdam';
+
     return res.json(mock1111)
   }
   if (barcodeId === '222222222222222') {
     setTimeout(() => {
-      const html = readMockHtml('mock-222222222222222.html');
-      const data = htmlToJson(html);
-      return res.json(data);
+      mock1111.zoekresultaat.Identificatienummer = barcodeId;
+      mock1111.zoekresultaat.Diernaam = 'Rocky';
+      mock1111.zoekresultaat.Diersoort = 'Hond';
+      mock1111.zoekresultaat.Woonplaats_houder = 'Amsterdam';
+      return res.json(mock1111)
     }, 5000); // 5 intentional latency
     return;
   }
   if (barcodeId === '333333333333333') {
-    const html = readMockHtml(`mock-333333333333333.html`);
-    const $ = cheerio.load(html);
-    const message = $('h1').text().trim();
-    return res.status(403).json({ message });
+    return res.status(404).json({ message: 'Toegang geweigerd!' });
   }
 
-  try {
-    const response = await axios.get(`https://ndg.nl/frm/fsearch.php?barcode=${barcodeId}`);
-    const html = response.data;
-    //console.log(`Response ` + html)
+  mock1111.zoekresultaat.Identificatienummer = barcodeId;
+  mock1111.zoekresultaat.Diernaam = 'Bob';
+  mock1111.zoekresultaat.Diersoort = 'Kat';
+  mock1111.zoekresultaat.Woonplaats_houder = 'Den Haag';
 
-    //Check for error message
-    const containsString = html.includes("Toegang geweigerd");
-    if(containsString){
-      console.log(`Toegang geweigerd!`)
-      return res.status(403).json({ message: 'Toegang geweigerd!' });
-    }
+  return res.json(mock1111)
+
+  // if (barcodeId === '111111111111111') {
+  //   return res.json(mock1111)
+  // }
+  // if (barcodeId === '222222222222222') {
+  //   setTimeout(() => {
+  //     const html = readMockHtml('mock-222222222222222.html');
+  //     const data = htmlToJson(html);
+  //     return res.json(data);
+  //   }, 5000); // 5 intentional latency
+  //   return;
+  // }
+  // if (barcodeId === '333333333333333') {
+  //   const html = readMockHtml(`mock-333333333333333.html`);
+  //   const $ = cheerio.load(html);
+  //   const message = $('h1').text().trim();
+  //   return res.status(403).json({ message });
+  // }
+
+  // try {
+  //   const response = await axios.get(`https://ndg.nl/frm/fsearch.php?barcode=${barcodeId}`);
+  //   const html = response.data;
+  //   //console.log(`Response ` + html)
+
+  //   //Check for error message
+  //   const containsString = html.includes("Toegang geweigerd");
+  //   if(containsString){
+  //     console.log(`Toegang geweigerd!`)
+  //     return res.status(403).json({ message: 'Toegang geweigerd!' });
+  //   }
     
-    //Convert html to json
-    const data = htmlToJson(html);
-    return res.json(data);
+  //   //Convert html to json
+  //   const data = htmlToJson(html);
+  //   return res.json(data);
 
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({message: 'Error fetching data'});
-  }
+  // } catch (error) {
+  //   console.error(error);
+  //   res.status(500).send({message: 'Error fetching data'});
+  // }
 
 })
 
 //------------------------------------------------
-const htmlToJson = (html) => {
-  const $ = cheerio.load(html);
+// const htmlToJson = (html) => {
+//   const $ = cheerio.load(html);
 
-  //Extract data
-  const data = {
-    source: "NDG",
-    description: "De Stichting Nederlandse Databank Gezelschapsdieren",
-    zoekresultaat: {
-      Identificatienummer: $('td.frmLabel:contains("Identificatienummer")').next('td.frmField').text(),
-      Diernaam: $('td.frmLabel:contains("Diernaam")').next('td.frmField').text(),
-      Diersoort: $('td.frmLabel:contains("Diersoort")').next('td.frmField').text(),
-      Paspoort_geregistreerd: $('td.frmLabel:contains("Paspoort geregistreerd?")').next('td.frmField').text(),
-      Woonplaats_houder: $('td.frmLabel:contains("Woonplaats houder")').next('td.frmField').text(),
-      Land_houder: $('td.frmLabel:contains("Land houder")').next('td.frmField').text(),
-      Telefoon_Nederland: $('td.frmLabel:contains("Telefoon (vanuit Nederland)")').next('td.frmField').text(),
-      Telefoon_buitenland: $('td.frmLabel:contains("Telefoon (vanuit buitenland)")').next('td.frmField').text(),
-      Vermist: $('td.frmLabel:contains("Vermist?")').next('td.frmField').text(),
-      Overleden: $('td.frmLabel:contains("Overleden?")').next('td.frmField').text()
-    }
-  };  
-  return data;
-};
+//   //Extract data
+//   const data = {
+//     source: "NDG",
+//     description: "De Stichting Nederlandse Databank Gezelschapsdieren",
+//     zoekresultaat: {
+//       Identificatienummer: $('td.frmLabel:contains("Identificatienummer")').next('td.frmField').text(),
+//       Diernaam: $('td.frmLabel:contains("Diernaam")').next('td.frmField').text(),
+//       Diersoort: $('td.frmLabel:contains("Diersoort")').next('td.frmField').text(),
+//       Paspoort_geregistreerd: $('td.frmLabel:contains("Paspoort geregistreerd?")').next('td.frmField').text(),
+//       Woonplaats_houder: $('td.frmLabel:contains("Woonplaats houder")').next('td.frmField').text(),
+//       Land_houder: $('td.frmLabel:contains("Land houder")').next('td.frmField').text(),
+//       Telefoon_Nederland: $('td.frmLabel:contains("Telefoon (vanuit Nederland)")').next('td.frmField').text(),
+//       Telefoon_buitenland: $('td.frmLabel:contains("Telefoon (vanuit buitenland)")').next('td.frmField').text(),
+//       Vermist: $('td.frmLabel:contains("Vermist?")').next('td.frmField').text(),
+//       Overleden: $('td.frmLabel:contains("Overleden?")').next('td.frmField').text()
+//     }
+//   };  
+//   return data;
+// };
 
 //------------------------------------------------
 
 // Function to read mock.html
-const readMockHtml = (file) => {
-  const filePath = path.join('./mock-data/', file);
-  return fs.readFileSync(filePath, 'utf8');
-};
+// const readMockHtml = (file) => {
+//   const filePath = path.join('./mock-data/', file);
+//   return fs.readFileSync(filePath, 'utf8');
+// };
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
